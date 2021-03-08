bc794e084c05f8eb2185153c8050f3d1d730cd19
unknown <DIGITAL@ISG-DIGITAL.innograph.com>
2021-03-08 22:39:35 +0700

    Add Market V-2

---
+++ Market V-2/MarketV-2.1.js
@@ -0,0 +1,17 @@
+let product = [{ id:'157902927272723', category:'Fast Food', Nama:'Noodle',
+                 price:'20000', stock:'10'},
+               { id:'157902927272750', category: 'Electronic', Nama:'Earpods',
+                 price:'500000', stock:'5'},
+               { id:'157902927272769', category: 'Cloth', Nama:'Hoodie',
+                 price:'50000', stock:'7'},
+              ] 
+
+
+function inputData() {
+
+}   
+
+function filterData() {
+
+}
+
